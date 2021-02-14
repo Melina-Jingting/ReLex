94a5cda027945c964a6cbf1a50efeda1540a785e
Melina-Jingting <melinajingting@gmail.com>
2021-02-14 14:24:50 +0800

    deleted resumes

--- resumes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Resume
-
-
-class ResumeAdmin(admin.ModelAdmin):
-    list_display = ('skills', 'experience')
-
-admin.site.register(Resume, ResumeAdmin)
--- resumes/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Resume(models.Model):
-    skills = models.CharField(max_length=255)
-    experience = models.CharField(max_length=255)
